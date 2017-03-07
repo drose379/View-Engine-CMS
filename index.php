46776c6c639c2f3ebd036ad8116a00e7068520b7
Dylan <dylanrose60@gmail.com>
2017-03-07 16:15:06 -0500

    Testing

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 
 $path = $_SERVER["DOCUMENT_ROOT"] . "View-Engine-CMS/ViewEngine.php";
-set_include_path($_SERVER["DOCUMENT_ROOT"] . "View-Engine-CMS/")
+set_include_path($_SERVER["DOCUMENT_ROOT"] . "View-Engine-CMS/");
 require $path;
