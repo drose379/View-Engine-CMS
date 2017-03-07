ef0cd5c79f0977768165134e7cc8b5fb6c9a8344
Dylan <dylanrose60@gmail.com>
2017-03-07 16:18:41 -0500

    Fix

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
 <?php
 
-$path = $_SERVER["DOCUMENT_ROOT"] . "View-Engine-CMS/ViewEngine.php";
-set_include_path($_SERVER["DOCUMENT_ROOT"] . "View-Engine-CMS/");
-require $path;
+require 'ViewEngine.php';
