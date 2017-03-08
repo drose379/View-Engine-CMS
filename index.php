7cfd28e3e743fea0bad69212b4b0a4af7eee8398
Dylan <dylanrose60@gmail.com>
2017-03-07 22:38:45 -0500

    Testing lists

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 require 'ViewEngine.php';
-$view = new ViewEngine( "testTemplate.php" );
+$view = new ViewEngine( "testTemplate.html" );
 $view->setValue( "header", "This is the header!" );
 $view->setValue( "items", array( "Brittany", "Dylan" ) );
 $view->view();
